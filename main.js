4f98d93be1dbfb01b578e6fd17d870c018b7ef94
sadiaromi <sadiaroman28@gmail.com>
2024-05-29 19:31:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// ============================================ Q# 1 ===============================
+var miles = 130;
+var conversionFactor = 1.60934;
+var kilometers = miles * conversionFactor;
+console.log("The distance of 130 kilometers is ".concat(kilometers));
---
+++ main.ts
@@ -0,0 +1,9 @@
+// ============================================ Q# 1 ===============================
+    
+
+let miles: number = 130;
+
+const conversionFactor = 1.60934;
+
+let kilometers = miles * conversionFactor;
+console.log(`The distance of 130 kilometers is ${kilometers}`);
